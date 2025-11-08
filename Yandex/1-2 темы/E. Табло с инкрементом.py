85d7ad062095d5f600404e2bf12aa1199c94fce4
Shamil65 <shamilziganshin132@gmail.com>
2025-11-08 22:22:18 +0500

    Долгооо)

--- Yandex/1-2 темы/E. Табло с инкрементом.py
+++ Yandex/1-2 темы/E. Табло с инкрементом.py
@@ -1,7 +1,9 @@
 n, k = map(int, input().split())
 
-for _ in range(k):
-    last_number = n % 10  # более быстрый способ
+for i in range(k):
+    n_str = str(n)
+    last_number = int(n_str[-1])
+
     n += last_number
 
 print(n)
