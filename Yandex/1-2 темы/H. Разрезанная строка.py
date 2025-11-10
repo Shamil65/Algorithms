6419e41734e39db0f34ab02531b32dd4e4e4246b
Shamil65 <shamilziganshin132@gmail.com>
2025-11-10 23:53:37 +0500

    Add data input

--- Yandex/1-2 темы/H. Разрезанная строка.py
+++ Yandex/1-2 темы/H. Разрезанная строка.py
@@ -1,2 +1,19 @@
+'''
+12 3
+cabacaqwerty
+erty
+caba
+caqw
+'''
+
 n, m = map(int, input().split())
 
+total_string = input()
+
+len_total_string = len(total_string)
+len_split_string = len_total_string/m
+
+all_strings = [input().strip() for _ in range(m)]
+
+
+print(all_strings)
