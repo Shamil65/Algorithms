d93e823d6c01a63054b7054cde7872ed2c3387fa
Shamil65 <shamilziganshin132@gmail.com>
2025-11-12 18:57:48 +0500

    готовый вариант H. Разрезанная строка.py

--- Yandex/1-2 темы/H. Разрезанная строка.py
+++ Yandex/1-2 темы/H. Разрезанная строка.py
@@ -14,15 +14,10 @@ def main():
         ppmi.append(i)
 
     assigned = []
-    print(pos_map)
     # Для каждого куска из входных данных находим его позицию
     for idx, piece in enumerate(pieces, start=1):
-        print(idx, piece)
-        print(pos_map[piece])
         pos = pos_map[piece].pop(0)  # достаем первую позицию
         assigned.append((pos, idx))
-        print("После", pos_map)
-    print("assigned", assigned)
     # Сортируем по позиции в исходной строке
     assigned.sort(key=lambda x: x[0])
     result = [str(idx) for _, idx in assigned]
